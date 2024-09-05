bbcec96c8c1f0e6d11dc57099c5ca636437a539d
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-09-05 15:09:05 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,11 +5,13 @@ int main(){
 
     int i, j;
 
-    for(i=5; i<=1; i--){
-        for(j=i; j>=i-1;j++){
-          cout<<"*";
+    for(i=5; i>=1; i--){
+
+        for(j=i; j>=1; j--){
+
+            cout << "*";
         }
-      cout<<"\n";
+        cout<< "\n";
     }
     return 0;
 }
