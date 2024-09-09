a6d892d85643a0f6eb9847422adb0f96bc8ab0a7
S-Puranik <132969156+S-Puranik@users.noreply.github.com>
2024-09-10 04:11:52 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i, j, k;
+
+    for(i=5; i>=1; i--){
+
+        for(j=i; j>=1; j--){
+
+            cout<< j;
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
